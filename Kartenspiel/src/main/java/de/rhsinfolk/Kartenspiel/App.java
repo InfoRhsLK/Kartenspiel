c7db2cad290f1774ae54a40baa041a92feebca85
Jan-Hendrik <jan-hendrik.peter@web.de>
2018-11-17 11:00:54 +0100

    Initial Commit

---
+++ Kartenspiel/src/main/java/de/rhsinfolk/Kartenspiel/App.java
@@ -0,0 +1,13 @@
+package de.rhsinfolk.Kartenspiel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
